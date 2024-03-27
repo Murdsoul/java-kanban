df6ac3e89c422d2794207337c90922f51f299485
Boris Zyablitsev <maniac963@mail.ru>
2024-03-28 02:40:24 +0700

    Доработка по 6 спринту

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,82 @@
-import managers.InMemoryTaskManager;
-import model.*;
-
+import managers.Managers;
+import managers.TaskManager;
+import model.Epic;
+import model.Subtask;
+import model.Task;
+import model.TaskStatus;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        InMemoryTaskManager manager = new InMemoryTaskManager();
+        TaskManager manager = Managers.getDefault();
+        createAllTasks(manager);
+        System.out.println("\n===Печать всех задач/эпиков/подзадач изначальный===");
+        printAllTasks(manager);
+    }
 
+
+    public static void createAllTasks(TaskManager manager) {
+        // создание 3-х задач
         Task task1 = new Task("1Task", "1TaskDescription", 0L, TaskStatus.NEW);
         manager.createTask(task1);
-        Task task2 = new Task("2Task", "2TaskDescription", 0L, TaskStatus.IN_PROGRESS);
+        Task task2 = new Task("2Task", "2TaskDescription", 0L, TaskStatus.NEW);
         manager.createTask(task2);
+        Task task3 = new Task("3Task", "3TaskDescription", 0L, TaskStatus.NEW);
+        manager.createTask(task3);
 
-
-        Epic epic1 = new Epic ("1Epic", "1EpicDescription", 0L, TaskStatus.NEW, new ArrayList<>());
+        // создание Эпика с 3-мя подзадачами
+        Epic epic1 = new Epic("1Epic", "1EpicDescription", 0L, TaskStatus.NEW, new ArrayList<>());
         manager.createEpic(epic1);
-        Subtask subtask11 = new Subtask("11SubTask", "11SubTaskDescription", 0L, TaskStatus.NEW,3L);
-        manager.createSubtask(subtask11);
-        Subtask subtask12 = new Subtask("12SubTask", "12SubTaskDescription", 0L, TaskStatus.NEW,3L);
-        manager.createSubtask(subtask12);
+        Subtask subTask11 = new Subtask("11SubTask", "11SubTaskDescription", 0L, TaskStatus.NEW, 4L);
+        manager.createSubtask(subTask11);
+        Subtask subTask12 = new Subtask("12SubTask", "12SubTaskDescription", 0L, TaskStatus.NEW, 4L);
+        manager.createSubtask(subTask12);
+        Subtask subTask13 = new Subtask("13SubTask", "13SubTaskDescription", 0L, TaskStatus.NEW, 4L);
+        manager.createSubtask(subTask13);
 
+        // создание Эпика без подзадач
         Epic epic2 = new Epic("2Epic", "2EpicDescription", 0L, TaskStatus.NEW, new ArrayList<>());
         manager.createEpic(epic2);
-        Subtask subtask21 = new Subtask("21SubTask", "21SubTaskDescription", 0L, TaskStatus.NEW,8L);
-        manager.createSubtask(subtask21);
 
+    }
+
+    public static void checkHistory(TaskManager manager) {
+        createHistory(manager);
+
+        manager.deleteTaskById(1L);
+        manager.deleteSubtaskById(5L);
+        System.out.println("\n===Печать истории просмотров после удаления задачи и подзадачи===");
+        System.out.println(manager.getHistory());
+
+        manager.deleteEpicById(4L);
+        System.out.println("\n===Печать истории просмотров после удаления эпика===");
+        System.out.println(manager.getHistory()); // проверить, что подзадачи 6 и 7 тоже удалились
+    }
+
+    public static void createHistory(TaskManager manager) {
+        manager.getSubtaskById(5L);
+        manager.getEpicById(4L);
+        manager.getTaskById(1L);
+        manager.getSubtaskById(5L);
+        manager.getEpicById(8L);
+        manager.getTaskById(2L);
+        manager.getTaskById(2L);
+        manager.getSubtaskById(6L);
+        manager.getEpicById(8L);
+        manager.getSubtaskById(7L);
+        manager.getTaskById(2L);
+        manager.getTaskById(1L);
+        manager.getEpicById(8L);
+
+        System.out.println("\n===Печать истории просмотров===");
+        System.out.println(manager.getHistory());
+    }
+
+    public static void printAllTasks(TaskManager manager) {
         System.out.println(manager.getAllTasks());
         System.out.println(manager.getAllEpics());
         System.out.println(manager.getAllSubtasks());
-
-        task1.setStatus(TaskStatus.IN_PROGRESS);
-        subtask11.setStatus(TaskStatus.IN_PROGRESS);
-        subtask12.setStatus(TaskStatus.DONE);
-        manager.updateTask(task1);
-        manager.updateSubtask(subtask11);
-        manager.updateSubtask(subtask12);
-
-        System.out.println(manager.getTaskById(1L));
-        System.out.println(manager.getSubtaskById(4L));
-        System.out.println(manager.getSubtaskById(5L));
-
-        manager.deleteTaskById(2L);
-        manager.deleteEpicById(6L);
-
-        manager.updateEpic(epic1);
-
-        System.out.println(manager.getEpicById(3L));
-        System.out.println(manager.getSubtasksByEpicId(3L));
-
     }
+
 }
